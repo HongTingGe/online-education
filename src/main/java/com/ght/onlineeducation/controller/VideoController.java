f9c2c89031abf337ee014ee9c6c48f35d1d4895c
HongTingGe <programmerting@163.com>
2019-11-27 23:17:13 +0800

    Add PageHelper

--- src/main/java/com/ght/onlineeducation/controller/VideoController.java
+++ src/main/java/com/ght/onlineeducation/controller/VideoController.java
@@ -2,10 +2,14 @@ package com.ght.onlineeducation.controller;
 
 import com.ght.onlineeducation.domain.Video;
 import com.ght.onlineeducation.service.VideoService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/video")
@@ -15,9 +19,18 @@ public class VideoController {
     private VideoService videoService;
 
     @GetMapping("/find/all")
-    public List<Video> pageVideo(@RequestParam(value = "page",defaultValue = "1")int page,
+    public Object pageVideo(@RequestParam(value = "page",defaultValue = "1")int page,
                                  @RequestParam(value = "size",defaultValue = "10")int size){
-        return videoService.findAll();
+
+        PageHelper.startPage(page, size);
+        List<Video> list = videoService.findAll();
+        PageInfo<Video> pageInfo = new PageInfo<>(list);
+        Map<String,Object> map = new HashMap<>();
+        map.put("data",list);
+        map.put("total_size",pageInfo.getTotal());
+        map.put("total_page",pageInfo.getPages());
+        map.put("current_page",page);
+        return map;
     }
 
     @GetMapping("/find/{id}")
